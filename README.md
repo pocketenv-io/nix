6840425f3ffb31e07ac13757150ee4797523c296
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-02-02 20:09:41 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Nix Workspace
+
+This is a template workspace with nix. It is a starting point for creating a new workspace.
+
---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "hostname" {
+  type = string
+  description = "Hostname of the container"
+  default = null
+} 
+
+variable "image" {
+  type = string
+  description = "Image to use, required if context is not set"
+  default = "ghcr.io/pocketenv-io/nix:main"
+}
+
+variable "workspace_name" {
+  type = string
+  description = "Name of the workspace"
+  default = "nix-workspace"
+}
+
+variable "user" {
+  type = string
+  description = "User to create"
+  default = "coder"
+}
+
+variable "context" {
+  type = string
+  description = "Context to use"
+  default = null
+}
+
+variable "volumes" {
+  type = map(string)
+  description = "Volumes to mount"
+  default = {
+    nix-workspace = "/home/coder"
+  }
+}
